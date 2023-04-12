192d76a3d6633fb9fdf466e559a1f4ebeff1b73c
Gheritarish <ilissiae@gmail.com>
2023-04-12 18:36:55 +0200

    feat(api): Allow filtering on name and ratings
    We allow a filtering on game name and ratings. This filtering is strict
    for now.

--- hlgames/games/urls.py
+++ hlgames/games/urls.py
@@ -1,13 +1,17 @@
 from rest_framework import routers
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 from games import views
 
 router = routers.DefaultRouter()
-router.register(r"games", views.GameViewSet)
 router.register(r"platforms", views.PlatformViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
+    re_path(
+        "games/",
+        views.GameViewSet.as_view(),
+        name="list_games",
+    ),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
--- hlgames/games/views.py
+++ hlgames/games/views.py
@@ -1,12 +1,24 @@
-from rest_framework import viewsets
+from rest_framework import generics, viewsets
 
 from games.serializer import GameSerializer, PlatformSerializer
 from games.models import Game, Platform
 
 
-class GameViewSet(viewsets.ModelViewSet):
-    queryset = Game.objects.all().select_related()
+class GameViewSet(generics.ListAPIView):
     serializer_class = GameSerializer
+
+    def get_queryset(self):
+        queryset = Game.objects.select_related()
+
+        name = self.request.query_params.get("name")
+        if name:
+            queryset = queryset.filter(name=name)
+
+        ratings = self.request.query_params.get("ratings")
+        if ratings:
+            queryset = queryset.filter(ratings=ratings)
+
+        return queryset
 
 
 class PlatformViewSet(viewsets.ModelViewSet):
